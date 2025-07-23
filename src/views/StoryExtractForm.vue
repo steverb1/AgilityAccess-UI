868fe462bbf1057eb97aa6860bd35efa25c09e29
steverb1 <steverb@mindspring.com>
2025-07-23 12:16:44 -0600

    removing test text

--- src/views/StoryExtractForm.vue
+++ src/views/StoryExtractForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="story-extract-form">
-    <h2>Extract Stories - test</h2>
+    <h2>Extract Stories</h2>
 
     <div v-if="status.message" :class="['status-message', status.type]">
       {{ status.message }}
